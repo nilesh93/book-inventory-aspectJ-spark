f76d99568d555a255702e053be49c1f2db539821
Nilesh Jayanandana <nilesh.jayanandana@yahoo.com>
2017-04-27 14:46:51 +0530

    configured Book CRUD

--- src/main/java/com/mtit/App.java
+++ src/main/java/com/mtit/App.java
@@ -5,6 +5,11 @@ import com.mtit.service.BookService;
 import com.mtit.service.BookServiceImpl;
 import com.mtit.service.UserService;
 import com.mtit.service.UserServiceImpl;
+import spark.ModelAndView;
+import spark.template.jade.JadeTemplateEngine;
+
+import java.util.HashMap;
+import java.util.Map;
 
 import static spark.Spark.*;
 import static spark.Spark.get;
@@ -43,6 +48,12 @@ public class App {
         post("/login", (req, res) -> gson.toJson(userService.getUser(req.body(), res)));
         post("/register", (req, res) ->  gson.toJson(userService.create(req.body(), res)));
 
+        Map<String, String> map = new HashMap<>();
+        map.put("message", "Hello World!");
+        get("/hello", (rq, rs) -> new ModelAndView(map, "hello"), new JadeTemplateEngine());
 
+        // login
+        get("/", (req, res) -> new ModelAndView(new HashMap<>(), "login"), new JadeTemplateEngine());
+        get("/home", (req, res) -> new ModelAndView(new HashMap<>(), "home"), new JadeTemplateEngine());
     }
 }
